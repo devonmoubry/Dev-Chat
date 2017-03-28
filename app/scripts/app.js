72dd93740442545a6b66aad9ea351bacc8197ac4
Devon Moubry <devon@moubry.com>
2017-03-28 15:00:37 -0500

    adding skeleton of prototypes, functions, class of hidden and seen

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,63 @@
 export default function app() {
-  let session;
-	const moment = require('moment');
-	 console.log(moment().format());
-	// all your codes are belong to us
+    let session;
+    let username;
+  	const moment = require('moment');
+  	 console.log(moment().format());
+  	// all your codes are belong to us
+    function Session(username) {
+      'use strict';
 
-	const $usernameSubmit = $('.submitUsername');
-	$usernameSubmit.submit(function(event) {
-		event.preventDefault();
-		session = new Session(event.target[0].value);
-		console.log(session);
-	});
+      this.username = username;
+    }
+
+    function Message (sender, body, timestamp) {
+      'use strict'
+
+      this.sender = sender;
+      this.body = body;
+      this.timestamp = timestamp;
+    }
+
+    Message.prototype.save = function() {
+
+    }
+
+    Message.prototype.delete = function() {
+
+    }
+
+    function renderLogin() {
+    	const $usernameSubmit = $('.submitUsername');
+    	$usernameSubmit.submit(function(event) {
+    		event.preventDefault();
+        username = event.target[0].value;
+    		session = new Session(username);
+
+    		console.log(session);
+
+    	});
+    }
+
+    function renderChat() {
+      //$('.loginContainer').remove();
+      //
+      // let settings = {
+      //   type: 'GET',
+      //   dataType: 'json',
+      //   url: 'http://tiny-za-server.herokuapp.com/collections/devonmoubry-devchat'
+      // }
+      //
+      // $.ajax(settings).then(function(data, status, xhr){
+      //   data.forEach(function(sender, body, timestamp) {
+      //     $('ul').append('<li><div class="sender"> + "Devon" + </div><div class="body"> + "Hello there" + </div><div class="timestamp"> + "beer o’clock" + </div><a id="delete"><i class="fa fa-trash-o" aria-hidden="true"></i></a></li>');
+      //   })
+      // })
+    }
+
+    function renderMessage() {
+
+    }
+  renderLogin();
 }
 
 // http://tiny-za-server.herokuapp.com/collections/devonmoubry-devchat
