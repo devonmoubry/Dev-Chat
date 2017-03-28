c5a7288362fda903ef0b2218e1f520a361e968ad
Devon Moubry <devon@moubry.com>
2017-03-27 21:46:14 -0500

    installed moment and added webpack, created session constructor to track username, added submit function to submit button

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,22 @@
 export default function app() {
+  let session;
+	var moment = require('moment');
+	 console.log(moment().format());
 	// all your codes are belong to us
+	function Session(username) {
+		'use strict';
+
+		this.username = username;
+	}
+
+	const $usernameSubmit = $('.submitUsername');
+	$usernameSubmit.submit(function(event) {
+		event.preventDefault();
+		session = new Session(event.target[0].value);
+		console.log(session);
+	});
+
 	console.log('tiy is awesome!')
 }
 
-var moment = require('moment');
-moment().format();
+// http://tiny-za-server.herokuapp.com/collections/devonmoubry-devchat
